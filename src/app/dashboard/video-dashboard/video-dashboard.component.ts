79be6e8d0049f566fc1a9b23771e926c67223194
John Baur <john.baur@oasisdigital.com>
2020-08-12 14:53:17 -0500

    Move video list to dashboard

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Video, mockVideoList } from '../../app-types';
+
 @Component({
   selector: 'jb-video-dashboard',
   templateUrl: './video-dashboard.component.html',
   styleUrls: ['./video-dashboard.component.css']
 })
 export class VideoDashboardComponent implements OnInit {
+  videos: Video[] = mockVideoList;
 
   constructor() { }
 
